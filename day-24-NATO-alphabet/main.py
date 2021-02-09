81bd5c2ad4f8d19a4904cdc6e7995f4eb8ffa42b
Aakash Chahal <aakash.chahal62@gmail.com>
2021-02-09 22:08:30 +0530

    added try, except to catch exceptions

--- day-24-NATO-alphabet/main.py
+++ day-24-NATO-alphabet/main.py
@@ -2,10 +2,16 @@
 # {new_key:new_value for (index, row) in df.iterrows()}
 import pandas
 
-data = pandas.read_csv("nato_phonetic_alphabet.csv
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(phonetic_dict)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+while True:
+    try:
+        word = input("Enter a word: ").upper()
+        output_list = [phonetic_dict[letter] for letter in word]
+    except:
+        print("Alphabets only")
+    else:
+        print(output_list)
+        break
