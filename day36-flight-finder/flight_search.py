35bf5d2e508d6a64e36fea4d7a50f66d9a251f26
Aakash Chahal <aakash.chahal62@gmail.com>
2021-02-20 18:01:49 +0530

    update on day37 fetched price using tequila api

--- day36-flight-finder/flight_search.py
+++ day36-flight-finder/flight_search.py
@@ -1,6 +1,8 @@
 import requests
 from flight_data import HEADERS
 from datetime import datetime, timedelta
+from notification_manager import NotificationManager
+notifier = NotificationManager()
 tequila_endpoint = "https://tequila-api.kiwi.com/v2/search"
 
 
@@ -9,6 +11,7 @@ class FlightSearch:
         self.parameters = {}
         self.date_from = (datetime.now() + timedelta(days=1)).strftime("%d/%m/%Y")
         self.date_to = (datetime.now() + timedelta(days=(6 * 30))).strftime("%d/%m/%Y")
+        self.sms = None
         self.response = None
 
     def search_flight(self, data):
@@ -26,6 +29,6 @@ class FlightSearch:
         # print(self.response)
         flight_price = self.response.json()['data'][0]['price']
         if flight_price <= data['lowestPrice']:
-            print(f"{data['city']}: ₹{flight_price}")
-            print(f"Booking link: {self.response.json()['data'][0]['deep_link']}")
-            print("-------------------------------------------------------")
+            self.sms = f"""{data['city']}: ₹{flight_price}
+            Booking link: {self.response.json()['data'][0]['deep_link']}"""
+            notifier.send_sms(self.sms)
