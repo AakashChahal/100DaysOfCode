84aca4c913d823ee010b1d7310f763184ef75f21
Aakash Chahal <aakash.chahal62@gmail.com>
2021-03-29 19:23:43 +0530

    JS added

---
+++ PracticeJavaScript/app.js
@@ -0,0 +1,28 @@
+// variables
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+const image = document.getElementById("my-img");
+let imageNum = 0;
+
+// functions
+const showNextImage = function () {
+    if (imageNum < 9) {
+        imageNum++;
+        image.src = `images/image${imageNum}.jpg`;
+    } else {
+        alert("You've reached the end");
+    }
+};
+
+const showPrevImage = function () {
+    if (imageNum > 0) {
+        imageNum--;
+        image.src = `images/image${imageNum}.jpg`;
+    } else {
+        alert("You've reached the end");
+    }
+};
+
+// events
+next.addEventListener("click", showNextImage);
+prev.addEventListener("click", showPrevImage);
