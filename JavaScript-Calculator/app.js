3d9e197adbb287e36509b736859b843062b93826
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-14 00:01:09 +0530

    Calculation logic : Start

--- JavaScript-Calculator/app.js
+++ JavaScript-Calculator/app.js
@@ -2,14 +2,35 @@
 
 // Variables
 let contOp = 0;
+let operator = "";
 const outputEl = document.getElementById("output");
-let output = Number(outputEl.textContent);
+let output = 0;
 const clearOutput = document.getElementById("clear");
 const backspace = document.getElementById("del");
 
 // Functions
 const evalExpression = function () {
-    console.log(output);
+    switch (operator) {
+        case "+":
+            output += Number(outputEl.textContent);
+            console.log(output);
+            break;
+        case "-":
+            output -= Number(outputEl.textContent);
+            console.log(output);
+            break;
+        case "×":
+            output *= Number(outputEl.textContent);
+            console.log(output);
+            break;
+        case "÷":
+            output /= Number(outputEl.textContent);
+            console.log(output);
+            break;
+
+        default:
+            break;
+    }
 };
 
 const showNumOnScreen = function (e) {
@@ -23,10 +44,30 @@ const showNumOnScreen = function (e) {
 const getOperator = function (e) {
     contOp++;
     if (contOp === 1) {
-        const operator = e.target.textContent;
-        const val = outputEl.textContent;
-        evalExpression(operator);
-        outputEl.textContent += operator;
+        operator = e.target.textContent;
+        const n = Number(outputEl.textContent);
+        switch (operator) {
+            case "+":
+                output += n;
+                console.log(output);
+                break;
+            case "-":
+                output -= n;
+                console.log(output);
+                break;
+            case "×":
+                output *= n;
+                console.log(output);
+                break;
+            case "÷":
+                output /= n;
+                console.log(output);
+                break;
+
+            default:
+                break;
+        }
+        outputEl.textContent = "";
     }
 };
 
@@ -39,4 +80,26 @@ document.querySelectorAll(".op").forEach((opEl) => {
     opEl.addEventListener("click", getOperator);
 });
 
-document;
+document.getElementById("eq").addEventListener("click", function () {
+    switch (operator) {
+        case "+":
+            output += Number(outputEl.textContent);
+            console.log(output);
+            break;
+        case "-":
+            output -= Number(outputEl.textContent);
+            console.log(output);
+            break;
+        case "×":
+            output *= Number(outputEl.textContent);
+            console.log(output);
+            break;
+        case "÷":
+            output /= Number(outputEl.textContent);
+            console.log(output);
+            break;
+
+        default:
+            break;
+    }
+});
