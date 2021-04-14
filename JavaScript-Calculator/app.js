843fade3f7d5f24b1f64ebbab44a6e347b801138
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-14 21:59:37 +0530

    Starting again with new logic

--- JavaScript-Calculator/app.js
+++ JavaScript-Calculator/app.js
@@ -1,81 +1,22 @@
 "use strict";
 
-// Variables
-// let contOp = 0;
-// let operator = "";
-const outputEl = document.getElementById("output");
-let output = 0;
-let val1 = 0;
-let val2 = 0;
-let operator = "";
-const clearOutput = document.getElementById("clear");
-const backspace = document.getElementById("del");
+// VARIABLES
+const inputNumber = document.getElementById("numbers");
+const operator = document.getElementById("operator");
+const tempOutput = document.getElementById("tempOut");
+const output = document.getElementById("output");
+const numpad = document.querySelectorAll(".num");
+const allOperators = document.querySelectorAll(".op");
+const equal = document.getElementById("eq");
 
-// Functions
-const evalExpression = function (e) {
-    val2 = Number(outputEl.textContent);
-    console.log(val1, val2, operator);
-    switch (operator) {
-        case "+":
-            output = val1 + val2;
-            console.log(output);
-            break;
-        case "-":
-            output = val1 - val2;
-            console.log(output);
-            break;
-        case "×":
-            output = val1 * val2;
-            console.log(output);
-            break;
-        case "÷":
-            output = val1 / val2;
-            console.log(output);
-            break;
-
-        default:
-            break;
-    }
-    outputEl.textContent = output;
+// FUNCTIONS
+const getNumberFromNumpad = (e) => {
+    inputNumber.textContent === "0"
+        ? (inputNumber.textContent = e.target.textContent)
+        : (inputNumber.textContent += e.target.textContent);
 };
 
-const showNumOnScreen = function (e) {
-    const n = e.target.textContent;
-    // contOp = 0;
-    outputEl.textContent == 0
-        ? (outputEl.textContent = n)
-        : (outputEl.textContent += n);
-};
-
-const getOperator = function (e) {
-    // contOp++;
-    val1 = Number(outputEl.textContent);
-    operator = e.target.textContent;
-    outputEl.textContent = "";
-};
-
-// Events
-document.querySelectorAll(".num").forEach((numEl) => {
-    numEl.addEventListener("click", showNumOnScreen);
-});
-
-document.querySelectorAll(".op").forEach((opEl) => {
-    opEl.addEventListener("click", getOperator);
-});
-
-document.getElementById("eq").addEventListener("click", evalExpression);
-
-clearOutput.addEventListener("click", function () {
-    outputEl.textContent = "";
-    val2 = 0;
-    val1 = 0;
-    output = 0;
-});
-
-backspace.addEventListener("click", function () {
-    console.log(typeof outputEl.textContent);
-    outputEl.textContent = outputEl.textContent.slice(
-        outputEl.textContent.length - 1,
-        1
-    );
+// EVENTS
+numpad.forEach((num) => {
+    num.addEventListener("click", getNumberFromNumpad);
 });
