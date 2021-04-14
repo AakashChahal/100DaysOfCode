aac66d90f8db0bbdae31347183585a978d6e2003
Aakash Chahal <aakash.chahal62@gmail.com>
2021-04-15 01:24:47 +0530

    First Solution

--- JavaScript-Calculator/app.js
+++ JavaScript-Calculator/app.js
@@ -1,10 +1,12 @@
 "use strict";
 
 // VARIABLES
+let currNumber = "";
+let currEval = 0;
 const inputNumber = document.getElementById("numbers");
 const operator = document.getElementById("operator");
 const tempOutput = document.getElementById("tempOut");
-const output = document.getElementById("output");
+const output = document.getElementById("answer");
 const numpad = document.querySelectorAll(".num");
 const allOperators = document.querySelectorAll(".op");
 const equal = document.getElementById("eq");
@@ -14,9 +16,91 @@ const getNumberFromNumpad = (e) => {
     inputNumber.textContent === "0"
         ? (inputNumber.textContent = e.target.textContent)
         : (inputNumber.textContent += e.target.textContent);
+    currNumber = Number(inputNumber.textContent);
+    // console.log(currNumber);
+};
+
+const currentOperator = (e) => {
+    inputNumber.textContent = "";
+    const currOp = e.target.textContent;
+    operator.textContent = currOp;
+    console.log(currNumber);
+    if (currEval === 0) {
+        currEval = currNumber;
+    } else {
+        switch (currOp) {
+            case "+":
+                currEval += Number(currNumber);
+                break;
+            case "-":
+                currEval -= Number(currNumber);
+                break;
+            case "×":
+                currEval *= Number(currNumber);
+                break;
+            case "÷":
+                currEval /= Number(currNumber);
+                break;
+
+            default:
+                break;
+        }
+    }
+    tempOutput.textContent = currEval;
+};
+
+const showAnswer = () => {
+    const currOp = operator.textContent;
+    // operator.textContent = currOp;
+    console.log(currNumber);
+    switch (currOp) {
+        case "+":
+            currEval += Number(currNumber);
+            break;
+        case "-":
+            currEval -= Number(currNumber);
+            break;
+        case "×":
+            currEval *= Number(currNumber);
+            break;
+        case "÷":
+            currEval /= Number(currNumber);
+            break;
+
+        default:
+            break;
+    }
+    inputNumber.textContent = "";
+    operator.textContent = "";
+    tempOutput.textContent = "";
+    output.textContent = currEval;
+    output.classList.remove("hide");
 };
 
 // EVENTS
 numpad.forEach((num) => {
     num.addEventListener("click", getNumberFromNumpad);
+});
+
+allOperators.forEach((op) => {
+    op.addEventListener("click", currentOperator);
+});
+
+equal.addEventListener("click", showAnswer);
+
+document.getElementById("clear").addEventListener("click", function () {
+    inputNumber.textContent = "0";
+    operator.textContent = "";
+    tempOutput.textContent = "";
+    currEval = 0;
+    output.textContent = "";
+    output.classList.add("hide");
+    console.log("done");
+});
+
+document.getElementById("del").addEventListener("click", function () {
+    inputNumber.textContent = inputNumber.textContent.slice(0, -1);
+    if (inputNumber.textContent === "") {
+        inputNumber.textContent = "0";
+    }
 });
